3923b4f54501e0157d11027e85a899c3baca9d45
tannerp6260 <tannerp6260@gmail.com>
2020-09-08 17:22:46 -0600

    implemented functions to check balance and difference in tree height

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include<iomanip>
+#include<algorithm>
 
 using namespace std;
 
@@ -39,6 +40,12 @@ BstNode* tempNode = NULL;
 
 BstNode* InOrder(BstNode* root);
 
+void delete_tree(BstNode* root); 
+
+int height(BstNode* root);
+
+int difference(BstNode* root);
+
 int main() {
 	BstNode* root = NULL;
 	char input;
@@ -62,6 +69,7 @@ int main() {
 	//}
 
 	while (1) {
+		cout << "\n------------------------------------------------------------------\n";
 		cout << "Choose the number of the option to be performed:\n";
 		cout << "\t1 Create Tree\n";
 		cout << "\t2 Insert data\n";
@@ -70,87 +78,95 @@ int main() {
 		cout << "\t5 Transversal\n";
 		cout << "\t6 Delete Tree\n";
 		cout << "\t7 Check Balance\n";
-
+		cout << "------------------------------------------------------------------\n";
 		input = getchar();
 
 		switch (input) {
 		case '1': {
-			cout << "Create Tree\n";
+			cout << "\n\n\n\tCreating Tree tree from file input......\n\n\n";
 			delete_tree();
 
 			root = NULL;
 
 			while (inFile >> x) {
-				cout << x;
 				root = Insert(root, x);
 			}
 			}
 				break;
 		case '2': {
-			cout << "Please enter the number you would like to add to the tree:\n";
+			cout << "\n\n\n\tPlease enter the number you would like to add to the tree:\n\n";
 			cin >> insertion_int;
-			cout << "------------------------------------------------------------------\n";
 			
 			root = Insert(root, insertion_int);
 		}
 				break;
 		case '3': {
 			if (root == NULL) {
-				cout << "Node cannot be deleted. Empty Tree!\n";
+				cout << "\n\n\n\t\tNode cannot be deleted. Empty Tree!\n\n";
 			}
 			else {
-				cout << "Please enter the number you want to delete\n";
+				cout << "\n\n\n\tPlease enter the number you want to delete\n\n";
 				cin >> delete_int;
 				Delete(root, delete_int);
 			}
 			break;
 		case '4': {
-			cout << "Search tree:\nPlease enter a number to search for:\n";
+			cout << "\n\n\n\tPlease enter a number to search for:\n\n";
 			cin >> search_int;
 
 			if (search_tree(root, search_int)) {
-				cout << "The number is in the tree";
+				cout << "\n\n\n\tThe number is in the tree\n\n\n";
 			}
 			else {
-				cout << "The number is NOT in the tree";
+				cout << "\n\n\n\tThe number is NOT in the tree\n\n\n";
 			}
 		}
 		}
 				break;
 		case '5': {
-			cout << "Enter the corresponding number to select traversal type:\n";
-			cout << "1 Pre-Order\n";
-			cout << "2 Post-Order\n";
-			cout << "3 In-Order\n";
-			//add error handling for cases where not 1,2,3
-			cin >> traversal_int;
-			cout << "------------------------------------------------------------------\n";
-
-			if (traversal_int == 1) {
-				PreOrder(root);
-			}
-			else if(traversal_int == 2){
-				PostOrder(root);
-			}
-			else if (traversal_int == 3) {
-				InOrder(root);
+			if (root == NULL) {
+				cout << "\n\n\tThere is no tree to traverse!\n\n\n";
 			}
 			else {
-				cout << "Error, please enter a valid number (1, 2, or 3)\n";
+				cout << "\n\n\n \tPlease enter the corresponding number to select traversal type:\n\n\n";
+				cout << "1 Pre-Order\n";
+				cout << "2 Post-Order\n";
+				cout << "3 In-Order\n\n";
+
+				cin >> traversal_int;
+
+				if (traversal_int == 1) {
+					PreOrder(root);
+				}
+				else if (traversal_int == 2) {
+					PostOrder(root);
+				}
+				else if (traversal_int == 3) {
+					InOrder(root);
+				}
+				else {
+					cout << "\n\n\n\tError, \n\n\n\tPlease enter a valid number (1, 2, or 3)\n\n\n";
+				}
 			}
+		}
+				break;
+		case '6': {
+			cout << "\n\n\n\tDeleting tree...\n\n\n";
+			delete_tree(root);
+			root = NULL;
 			}
 				break;
-			case '6':
-				break;
-			case '7':
+		case '7': {
+			cout << height(root);
+			cout << "\n";
+			cout << difference(root);
+			cout << "\n";
+			}
 				break;
 			default:
-				cout << "error";
+				cout << "\n\n\n\terror\n\n\n";
 		}
 		input = getchar();
-		//root = Insert(root, 15);
-		//root = Insert(root, 10);
-		//root = Insert(root, 20);
 	}
 
 	return 0;
@@ -167,7 +183,7 @@ BstNode* Insert(BstNode* root, int data) {
 		root->right = Insert(root->right, data);
 	}
 	else {
-		cout << "The selected number already exists!\n";
+		cout << "\n\n\n\tThe selected number already exists!\n\n\n";
 	}
 
 	return root;
@@ -213,8 +229,6 @@ BstNode* Delete(BstNode* root, int data) {
 	else {	//search is complete, now we need to handle deletion
 		//no child node
 		if (root->left == NULL && root->right == NULL) {
-			cout << "deleting node no child\n";
-			cout << root->data;
 			delete root;
 			root = NULL;
 				
@@ -222,23 +236,17 @@ BstNode* Delete(BstNode* root, int data) {
 		//one child node
 		else if (root->left == NULL) {
 			//BstNode* tempNode = new BstNode();
-			cout << "deleting node one child\n";
-			cout << root->data;
 			struct BstNode *temp = root;
 			root = root->right;
 			delete temp;
 		}
 		else if (root->right == NULL) {
-			cout << "deleting node right child\n";
-			cout << root->data;
 			struct BstNode* temp = root;
 			root = root->left;
 			delete temp;
 		}
 		//two children
 		else {
-			cout << "deleting node left child\n";
-			cout << root->data;
 			struct BstNode* temp = FindMin(root->right);
 			root->data = temp->data;
 			root->right = Delete(root->right, temp->data);
@@ -286,4 +294,35 @@ BstNode* InOrder(BstNode* root) {
 		InOrder(root->right);
 	}
 	return root;
+}
+
+void delete_tree(BstNode* root) {
+	if (root) {					//check to see if binary tree is empty
+		if (root->left) {
+			delete_tree(root->left);		//go to the lowest value
+		}
+		if (root->right) {
+			delete_tree(root->right);		//go to the lowest value on tree
+		}
+
+		delete root;
+	}
+}
+
+int difference(BstNode* root) {
+	int left_height = height(root->left);
+	int right_height = height(root->right);
+	int balance_factor = left_height - right_height;
+	return balance_factor;
+}
+
+int height(BstNode* root) {
+	int height_int = 0;
+	if (root != NULL) {
+		int left_height = height(root->left);
+		int right_height = height(root->right);
+		int max_height = max(left_height, right_height);
+		height_int = max_height + 1;
+	}
+	return height_int;
 }
